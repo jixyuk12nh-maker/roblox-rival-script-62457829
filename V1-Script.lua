58b48161d4f8647706467c1bf5ed7a5c797fcd18
jixyuk12nh-maker <jixyuk12nh@gmail.com>
2026-08-07 08:40:57 +0900

    Update V1-Script.lua

--- V1-Script.lua
+++ V1-Script.lua
@@ -2,5 +2,4 @@ if game.GameId ~= 6035872082 then
     return
 end
 
-loadstring(game:HttpGet(
-    "https://raw.githubusercontent.com/jixyuk12nh-maker/Rivals_script/main/jixyuk12nh.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/jixyuk12nh-maker/Rivals_script/main/jixyuk12nh.lua"))()
