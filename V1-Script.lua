abefce4b81934c4c8d103a99f1b74b874f8a0ac7
jixyuk12nh-maker <jixyuk12nh@gmail.com>
2026-08-07 08:39:32 +0900

    Update V1-Script.lua

--- V1-Script.lua
+++ V1-Script.lua
@@ -1,16 +1,6 @@
---!strict
-
-local HttpGet = game.HttpGet
-local GameId: number = game.GameId
-
-local Games: {[number]: string} = {
-    [6035872082] = "https://raw.githubusercontent.com/jixyuk12nh-maker/Rivals_script/main/jixyuk12nh.lua"
-}
-
-local URL: string? = Games[GameId]
-
-if not URL then
+if game.GameId ~= 6035872082 then
     return
 end
 
-loadstring(HttpGet(game, URL))()
+loadstring(game:HttpGet(
+    "https://raw.githubusercontent.com/jixyuk12nh-maker/Rivals_script/main/jixyuk12nh.lua"))()
